d357003623d8f767bc36c6e7b88904a0410af6dc
Christian Buenavista <Christian@propertyinabox.com.au>
2013-11-21 07:18:35 +0800

    admin message history fixed

--- thesis/application/views/admin.php
+++ thesis/application/views/admin.php
@@ -1,29 +1,10 @@
-<style>
-	.content-toggle {display:none}
-</style>
 <?php 
 	/* -----------------------------------------
 				For chat system 
 	   ---------------------------------------- */
-	session_start();
 	$login = $this->session->userdata('logged_in');
 	
-	if(!empty($login)) {
-		// Must be already set
-		$_SESSION['username'] = $login['username']; 
-	} else {
-		unset($_SESSION['username']);
-		session_destroy();
-	}
 ?>
-
-<?php if(!empty($login)): ?>
-	<!-- For Chat system -->
-    <link href="<?php echo base_url().'assets/front/css/screen.css'; ?>" rel="stylesheet" media="screen">
-    <link href="<?php echo base_url().'assets/front/css/chat.css'; ?>" rel="stylesheet" media="screen">
-    <script src="<?php echo base_url().'assets/front/js2/jquery.js';?>"></script>
-    <script src="<?php echo base_url().'assets/front/js2/chat.js';?>"></script>
-<?php endif; ?>    
 
 <?php include('admin/header/header_dropdown.php');?>
 <!-- Navigation Bar Section-->
