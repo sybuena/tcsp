d208a24e20d4f3017435ef56c560fa332456cbfe
Christian Buenavista <Christian@propertyinabox.com.au>
2013-12-07 18:34:13 +0800

    noooo

---
+++ thesis/application/controllers/email.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Email extends MY_Controller {
+	
+	
+
+ 
+	
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ thesis/application/libraries/MY_Email.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+	Email Class
+ *
+ * @property email_log_model $email_log_model
+ */
+class MY_Email extends CI_Email {
+
+	
+} // END CLASS
